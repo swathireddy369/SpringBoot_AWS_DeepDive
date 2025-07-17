637cc0cbfcfe1fc8ee4a2e9860ee681567dcc99e
Swathi Amaravadi <swathiamaravadi53@gmail.com>
2025-07-17 19:43:28 -0400

    Initial commit

---
+++ src/main/java/com/example/springboot_aws_deepdive/SpringBootAwsDeepDiveApplication.java
@@ -0,0 +1,20 @@
+package com.example.springboot_aws_deepdive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class SpringBootAwsDeepDiveApplication {
+    @GetMapping("/")
+    public String home(){
+        return "Welcome to AWS First Deployment.........!!!";
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootAwsDeepDiveApplication.class, args);
+    }
+
+}
